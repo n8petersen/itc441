d62bd38f590d5336d9b5f433f9647b62d1e274f4
n8petersen <n8knows@gmail.com>
2024-02-20 21:06:28 -0700

    Clean up formatting

--- Lab 3 - Vehicle Sensor/sensor/sensor.ino
+++ Lab 3 - Vehicle Sensor/sensor/sensor.ino
@@ -1,11 +1,11 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 #include <WiFiClient.h>
-#include "NewPing.h"  // Imported "NewPing" library by Tim Eckel <eckel.tim@gmail.com>
+#include "NewPing.h" // Imported "NewPing" library by Tim Eckel <eckel.tim@gmail.com>
 
 // WiFi Settings
-const char* wifiSSID = "nathan";
-const char* wifiPSK = "ilikepizza123";
+const char *wifiSSID = "nathan";
+const char *wifiPSK = "ilikepizza123";
 
 // Stoplight IP
 String serverName = "http://192.168.137.247";
@@ -24,86 +24,92 @@ NewPing sonar(TRIG, ECHO, MAX);
 
 bool toggle = true;
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
-  delay(5000);  // Give things time to start up
+  delay(5000); // Give things time to start up
   Serial.println("Initializing ...");
 
   // Set built-in LED to output and turn off
   pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(D4, HIGH);  // turn off the built in LED
+  digitalWrite(D4, HIGH); // turn off the built in LED
 
   // Connect to WiFi - Adapted from http://www.esp8266learning.com/wemos-webserver-example.php
   Serial.println("Connecting to ");
   Serial.println(wifiSSID);
   WiFi.begin(wifiSSID, wifiPSK);
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(500);
     Serial.print(".");
   }
   Serial.println("");
   Serial.println("WiFi connected");
-  digitalWrite(D4, LOW);  // turn the Built-in LED on after WiFi is connected and ready
+  digitalWrite(D4, LOW); // turn the Built-in LED on after WiFi is connected and ready
 }
 
-void loop() {
+void loop()
+{
 
-  if (WiFi.status() == WL_CONNECTED) {
+  if (WiFi.status() == WL_CONNECTED)
+  {
     WiFiClient client;
     HTTPClient http;
     String serverPath;
 
-
     // Get the distance using the NewPing library
     float distance = (sonar.ping() / 2) * 0.0343;
 
-    // print distance for debugging
-    // Serial.print("Dist: ");
-    // Serial.print(distance);
-    // Serial.println(" cm");
-
-    if (green > distance && distance >= yellow) {
-      // Serial.println("green");
+    if (green > distance && distance >= yellow)
+    {
       serverPath = serverName + "/green";
       toggle = true;
     }
 
-    else if (yellow > distance && distance >= red) {
-      // Serial.println("yellow");
+    else if (yellow > distance && distance >= red)
+    {
       serverPath = serverName + "/yellow";
       toggle = true;
     }
 
-    else if (red > distance && distance >= blink_red) {
-      // Serial.println("red");
+    else if (red > distance && distance >= blink_red)
+    {
       serverPath = serverName + "/red";
       toggle = true;
     }
 
-    else if (blink_red > distance && distance > 0) {
+    else if (blink_red > distance && distance > 0)
+    {
       // Serial.println("blink");
       serverPath = serverName + "/timer";
-      // send once and stop repeating it
-      Serial.print("dist: ");
-      Serial.print(distance);
 
-      Serial.print("  --  ");
-      Serial.print(serverPath);
+      // send once before we set toggle false, but don't repeat on subsequent runs
+      if (toggle)
+      {
+        Serial.print("dist: ");
+        Serial.print(distance);
 
-      http.begin(client, serverPath);
-      int httpResponseCode = http.GET();
+        Serial.print("  --  ");
+        Serial.print(serverPath);
 
-      Serial.print("  --  ");
-      Serial.println(httpResponseCode);
+        http.begin(client, serverPath);
+        int httpResponseCode = http.GET();
+
+        Serial.print("  --  ");
+        Serial.println(httpResponseCode);
+      }
+
       toggle = false;
     }
 
-    else {
+    else
+    {
       serverPath = serverName + "/off";
       toggle = true;
     }
 
-    if (toggle == true) {
+    if (toggle == true)
+    {
       Serial.print("dist: ");
       Serial.print(distance);
 
