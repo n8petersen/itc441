6db993bc19edfc2594b4af30b41d50b78ea98fdd
n8petersen <n8knows@gmail.com>
2024-03-06 22:41:02 -0700

    Convert to use MQTT

---
+++ Lab 4 - MQTT/distance/distance.ino
@@ -0,0 +1,141 @@
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include "NewPing.h"  // Imported "NewPing" library by Tim Eckel <eckel.tim@gmail.com>
+
+// Wifi + Broker Info
+const char* ssid = "nathan";
+const char* password = "ilikepizza123";
+const char* server = "192.168.137.1";
+const char* ledTopic = "/lights";
+const char* doorTopic = "/garage";
+const char* host = "arduino-distance";  // Create hostname for this device
+
+WiFiClient wifiClient;
+PubSubClient mqttClient(wifiClient);
+
+// Distances Settings (cm)
+const int blink_red = 10;
+const int red = 15;
+const int yellow = 20;
+const int green = 25;
+
+char* currColor = "off";
+char* newColor = "off";
+
+// Sensor setup
+#define ECHO D2
+#define TRIG D3
+#define MAX 400
+NewPing sonar(TRIG, ECHO, MAX);
+
+bool toggle = true;
+bool garageOpen = false;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Initializing ...");
+
+  // Set built-in LED to output and turn off
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(D4, HIGH);  // turn off the built in LED
+
+  // ## Connect to WiFi - Adapted from http://www.esp8266learning.com/wemos-webserver-example.php
+  Serial.print("Connecting WiFi to WiFi: ");
+  Serial.print(ssid);
+
+  WiFi.hostname(host);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {  //Wait for connection
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi Connected");
+  delay(2000);  // Give things time to start up properly
+
+  //  ## Connect to MQTT
+  Serial.println("Connecting to MQTT.");
+  mqttClient.setServer(server, 1883);
+  mqttClient.setCallback(callback);
+  mqttClient.connect(host);
+  mqttClient.subscribe(ledTopic);
+  Serial.println(mqttClient.state());
+
+  mqttClient.publish("/debug", "Sensor Connected");
+
+  digitalWrite(D4, LOW);  // turn on LED after WiFi and MQTT are connected
+}
+
+void loop() {
+
+  mqttClient.loop();
+
+  // Get the distance using the NewPing library
+  float distance = (sonar.ping() / 2) * 0.0343;
+
+  // print distance for debugging
+  // Serial.print("Dist: ");
+  // Serial.print(distance);
+  // Serial.println(" cm");
+
+  if (green > distance && distance >= yellow) {
+    newColor = "green";
+    // mqttClient.publish("/lights", "green");
+    // toggle = true;
+  }
+
+  else if (yellow > distance && distance >= red) {
+    newColor = "yellow";
+    // mqttClient.publish("/lights", "yellow");
+    // toggle = true;
+  }
+
+  else if (red > distance && distance >= blink_red) {
+    newColor = "red";
+    // mqttClient.publish("/lights", "red");
+    // toggle = true;
+  }
+
+  else if (blink_red > distance && distance > 0) {
+    newColor = "blink";
+    // mqttClient.publish("/lights", "blink");
+  }
+
+  else {
+    newColor = "off";
+    // mqttClient.publish("/lights", "off");
+    // toggle = true;
+  }
+
+  if (newColor != currColor) {
+    mqttClient.publish("/lights", newColor);
+    currColor = newColor;
+  }
+
+  delay(100);
+}
+
+void callback(char* topicChar, byte* payload, unsigned int length) {
+  String topic = (String)topicChar;  // Convert the char* to a String
+  String message = "";
+
+  for (int i = 0; i < length; i++) {  // For each character of the payload
+    message += (char)payload[i];      // append to the message string
+  }
+
+  Serial.print("Message arrived [");  // Serial Debug
+  Serial.print(topic);                //    Print the topic name [in brackets]
+  Serial.print("] ");                 //
+  Serial.println(message);            //    And the message
+
+  if (topic == (String)doorTopic) {
+    if (message == "open") {
+      garageOpen = true;
+    }
+
+    else {
+      garageOpen = true;
+    }
+  }
+}
---
+++ Lab 4 - MQTT/light/light.ino
@@ -0,0 +1,176 @@
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+// Wifi + Broker Info
+const char* ssid = "nathan";
+const char* password = "ilikepizza123";
+const char* server = "192.168.137.1";
+const char* ledTopic = "/lights";
+const char* host = "arduino-lights";  // Create hostname for this device
+
+WiFiClient wifiClient;
+PubSubClient mqttClient(wifiClient);
+
+int redLED = D3;
+int yellowLED = D2;
+int greenLED = D1;
+
+bool timerOn = false;
+unsigned long previousMillis = 0;
+long interval = 500;
+bool redblinkon = false;
+
+// ## Re-usable function to turn on a specific LED
+void turnLED(String color = "off") {
+  // Turn off all LED's
+  digitalWrite(redLED, LOW);
+  digitalWrite(yellowLED, LOW);
+  digitalWrite(greenLED, LOW);
+
+  // only turn on the passed LED
+  if (color == "red") {
+    digitalWrite(redLED, HIGH);
+  } else if (color == "yellow") {
+    digitalWrite(yellowLED, HIGH);
+  } else if (color == "green") {
+    digitalWrite(greenLED, HIGH);
+  }
+}
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Initializing ...");
+
+  // ## Set all color pins to output
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(redLED, OUTPUT);
+  pinMode(yellowLED, OUTPUT);
+  pinMode(greenLED, OUTPUT);
+
+  // ## Initialize all LED's to OFF
+  turnLED();
+  digitalWrite(D4, HIGH);  // turn off the built in LED
+
+  // ## Connect to WiFi - Adapted from http://www.esp8266learning.com/wemos-webserver-example.php
+  Serial.print("Connecting WiFi to WiFi: ");
+  Serial.print(ssid);
+
+  WiFi.hostname(host);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {  //Wait for connection
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi Connected");
+  delay(2000);  // Give things time to start up properly
+
+  //  ## Connect to MQTT
+  Serial.println("Connecting to MQTT.");
+  mqttClient.setServer(server, 1883);
+  mqttClient.setCallback(callback);
+  mqttClient.connect(host);
+  mqttClient.subscribe(ledTopic);
+  Serial.println(mqttClient.state());
+
+  mqttClient.publish("/debug", "Lights Connected");
+
+  digitalWrite(D4, LOW);  // turn on LED after WiFi and MQTT are connected
+}
+
+void loop() {
+
+  mqttClient.loop();
+
+  // // run the timer if it is turned on
+  if (timerOn) {
+    Serial.print("T");  // used for debugging to ensure it was actually on
+    unsigned long currentMillis = millis();
+    if (currentMillis - previousMillis >= interval) {
+      previousMillis = currentMillis;
+      turnLED();
+      redblinkon = !redblinkon;
+      digitalWrite(redLED, redblinkon);
+    }
+  }
+
+
+  // if (mqtt_color == "red") {
+  //   turnLED("red");
+  //   timerOn = false;
+  //   // redblinkon = false;
+  // }
+
+  // if (mqtt_color == "yellow") {
+  //   turnLED("yellow");
+  //   timerOn = false;
+  //   // redblinkon = false;
+  // }
+
+  // if (mqtt_color == "green") {
+  //   turnLED("green");
+  //   timerOn = false;
+  //   // redblinkon = false;
+  // }
+
+  // if (mqtt_color == "off") {
+  //   turnLED();
+  //   timerOn = false;
+  //   // redblinkon = false;
+  // }
+
+  // if (mqtt_color == "blink") {
+  //   if (!timerOn) {
+  //     timerOn = true;
+  //   }
+  // }
+  // delay(10);
+}
+
+void callback(char* topicChar, byte* payload, unsigned int length) {
+  String topic = (String)topicChar;  // Convert the char* to a String
+  String message = "";
+
+  for (int i = 0; i < length; i++) {  // For each character of the payload
+    message += (char)payload[i];      // append to the message string
+  }
+
+  Serial.print("Message arrived [");  // Serial Debug
+  Serial.print(topic);                //    Print the topic name [in brackets]
+  Serial.print("] ");                 //
+  Serial.println(message);            //    And the message
+
+  if (topic == (String)ledTopic) {
+    if (message == "red") {
+      turnLED("red");
+      timerOn = false;
+      // redblinkon = false;
+    }
+
+    if (message == "yellow") {
+      turnLED("yellow");
+      timerOn = false;
+      // redblinkon = false;
+    }
+
+    if (message == "green") {
+      turnLED("green");
+      timerOn = false;
+      // redblinkon = false;
+    }
+
+    if (message == "off") {
+      turnLED();
+      timerOn = false;
+      // redblinkon = false;
+    }
+
+    if (message == "blink") {
+      if (!timerOn) {
+        timerOn = true;
+      }
+    }
+  }
+}
